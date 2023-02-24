4b948d6b55c2cf1fbc5a8f1183b4c38de728d1c9
Semyon Rozhkov <semro@outlook.com>
2023-02-24 16:07:29 +0300

    ci: create-dirs

--- yandex-cloud/yc-get-cert-action/entrypoint.sh
+++ yandex-cloud/yc-get-cert-action/entrypoint.sh
@@ -9,38 +9,39 @@ echo $YC_SA_JSON_CREDENTIALS > key.json
 
 echo "Installing GitHub CLI"
 curl -Lo tmp/ghlinux.tar.gz \
+  --create-dirs \
   https://github.com/cli/cli/releases/download/v2.23.0/gh_2.23.0_linux_amd64.tar.gz \
   && tar --strip-components=1 -xf tmp/ghlinux.tar.gz \
   && rm tmp/ghlinux.tar.gz
 
 gh --help
 
-echo "Installing Yandex Cloud CLI..."
-curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | \
-    bash -s -- -i /opt/yandex-cloud -n
-PATH=$PATH:/opt/yandex-cloud/bin/
+# echo "Installing Yandex Cloud CLI..."
+# curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | \
+#     bash -s -- -i /opt/yandex-cloud -n
+# PATH=$PATH:/opt/yandex-cloud/bin/
 
-echo "Creating Service Account profile..."
-yc config profile create sa-profile
-yc config set service-account-key key.json
-yc config set folder-id $YC_FOLDER_ID
+# echo "Creating Service Account profile..."
+# yc config profile create sa-profile
+# yc config set service-account-key key.json
+# yc config set folder-id $YC_FOLDER_ID
 
-echo "Getting certificate..."
-yc certificate-manager certificate content --id $YC_CERTIFICATE_ID --chain certificate_full_chain.pem --key private_key.pem > /dev/null
-CERTIFICATE_FULL_CHAIN=`cat certificate_full_chain.pem`
-PRIVATE_KEY=`cat private_key.pem`
+# echo "Getting certificate..."
+# yc certificate-manager certificate content --id $YC_CERTIFICATE_ID --chain certificate_full_chain.pem --key private_key.pem > /dev/null
+# CERTIFICATE_FULL_CHAIN=`cat certificate_full_chain.pem`
+# PRIVATE_KEY=`cat private_key.pem`
 
-echo "Converting certificate to a single string..."
-CERTIFICATE_FULL_CHAIN=$(echo "$CERTIFICATE_FULL_CHAIN" | tr '\n' ';')
-PRIVATE_KEY=$(echo "$PRIVATE_KEY" | tr '\n' ';')
+# echo "Converting certificate to a single string..."
+# CERTIFICATE_FULL_CHAIN=$(echo "$CERTIFICATE_FULL_CHAIN" | tr '\n' ';')
+# PRIVATE_KEY=$(echo "$PRIVATE_KEY" | tr '\n' ';')
 
-echo "Sending certificate to Github Output..."
-echo "certificate-full-chain<<EOF" >> $GITHUB_OUTPUT
-echo "$CERTIFICATE_FULL_CHAIN" >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
+# echo "Sending certificate to Github Output..."
+# echo "certificate-full-chain<<EOF" >> $GITHUB_OUTPUT
+# echo "$CERTIFICATE_FULL_CHAIN" >> $GITHUB_OUTPUT
+# echo "EOF" >> $GITHUB_OUTPUT
 
-echo "private-key<<EOF" >> $GITHUB_OUTPUT
-echo "$PRIVATE_KEY" >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
+# echo "private-key<<EOF" >> $GITHUB_OUTPUT
+# echo "$PRIVATE_KEY" >> $GITHUB_OUTPUT
+# echo "EOF" >> $GITHUB_OUTPUT
 
-echo "Done!"
+# echo "Done!"
